c7c3b73178b12f7356d667456f81df88ca8057f6
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-30 09:32:40 +0200

    Initial commit

---
+++ src/main/java/mirkoabozzi/Abozzi/Market/AbozziMarketApplication.java
@@ -0,0 +1,13 @@
+package mirkoabozzi.Abozzi.Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbozziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbozziMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mirkoabozzi/Abozzi/Market/AbozziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package mirkoabozzi.Abozzi.Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbozziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
