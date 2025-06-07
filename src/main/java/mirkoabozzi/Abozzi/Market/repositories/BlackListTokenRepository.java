ba72c99418fc83a159601587207c7f7f7abab683
Mirko Abozzi <mirko.abozzi@gmail.com>
2025-06-07 14:34:09 +0200

    added banner

--- src/main/java/mirkoabozzi/Abozzi/Market/repositories/BlackListTokenRepository.java
+++ src/main/java/mirkoabozzi/Abozzi/Market/repositories/BlackListTokenRepository.java
@@ -2,10 +2,15 @@ package mirkoabozzi.Abozzi.Market.repositories;
 
 import mirkoabozzi.Abozzi.Market.entities.BlackListToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface BlackListTokenRepository extends JpaRepository<BlackListToken, UUID> {
     Optional<BlackListToken> findByToken(String token);
+
+    void deleteByCreatedAtBefore(LocalDateTime targetDate);
 }
